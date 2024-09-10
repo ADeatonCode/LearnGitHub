8ab57791041457ce83b7a577057ed549d2048d1e
ADeatonCode <acdelgato@gmail.com>
2024-09-10 18:54:35 -0500

    try it again

---
+++ gitcreate.sh
@@ -0,0 +1,7 @@
+echo "# LearnGitHub" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ADeatonCode/LearnGitHub.git
+git push -u origin main
